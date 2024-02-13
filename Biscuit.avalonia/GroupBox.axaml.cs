9112af137eecf4ee6dc374e46fbeaa96da4aae7d
whpark <45619217+whpark@users.noreply.github.com>
2024-02-13 17:19:17 +0900

    GroupBox

---
+++ Biscuit.avalonia/GroupBox.axaml.cs
@@ -0,0 +1,20 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Controls.Templates;
+using Avalonia.Markup.Xaml;
+using Avalonia.Media;
+
+namespace Biscuit.avalonia;
+
+public partial class xGroupBox : HeaderedContentControl
+{
+	//public static readonly StyledProperty<IControlTemplate> HeaderProperty
+	//	= AvaloniaProperty.Register<GroupBox, IControlTemplate>(nameof(Header));
+
+	//public object Header { get; set; }
+	//public object Content { get; set; }
+	public xGroupBox() {
+        InitializeComponent();
+    }
+}

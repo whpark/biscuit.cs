5fdeb8a8acde75ff6b8032386a2edcfe8bce6de9
whpark <whitedevil75@naver.com>
2025-04-30 17:02:30 +0900

    C# speed up by CV.Mat.Ptr()  // unsafe block

--- Biscuit.clr/ImageHelper.h
+++ Biscuit.clr/ImageHelper.h
@@ -47,6 +47,35 @@ namespace Biscuit {
 
 	public:
 		static BitmapData^ GetBitmapData(CV::Mat^ mat);
+
+		static bool PackValue(CV::Mat^ mat, uint32_t row0, uint32_t row1, int32_t bpp, array<int>^ buffer, int^ pos) {
+			//// make sure, bpp is one of 1, 2, 4
+			//if (bpp != 1 && bpp != 2 && bpp != 4)
+			//	return;
+			if (mat->Type() != CV::MatType::CV_8UC1)
+				return false;
+
+			const uint8_t shift0 = 32;
+			uint32_t mask = (1u << bpp) - 1;
+
+			for (uint32_t y = row0; y < row1; y++) {
+				int shift = shift0 - bpp;
+				IntPtr ptr = mat->Ptr(y);
+				for (int x = 0; x < mat->Cols; x++, shift -= bpp) {
+					uint32_t v = ((uint8_t*)ptr.ToPointer())[x];
+					buffer[*pos] |= (int)((v & mask) << (int)shift);
+					if (shift == 0) {
+						(*pos)++;
+						shift = shift0;
+					}
+				}
+				if (shift != shift0-bpp)
+					(*pos)++;
+			}
+
+			return true;
+		}
+
 	};
 
 }
